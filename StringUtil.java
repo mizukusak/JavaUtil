26e0ff7cce50d13d5d434a5c53864dea3c314054
fumiaki shimazu <blood.opposer@gmail.com>
2016-07-27 12:24:21 +0900

    Create StringUtil.java

---
+++ StringUtil.java
@@ -0,0 +1,26 @@
+public class StringUtil {
+	/**
+	 * 任意の文字を追加指定可能な trim
+	 * @param val
+	 * @param list
+	 * @return
+	 */
+	public static String trim(String val, char[] list) {
+		int len = val.length();
+		int st = 0;
+
+		while (st < len) {
+			char c = val.charAt(st);
+			if (c > ' ' && !ArrayUtil.in(list, c))
+				break;
+			st++;
+		}
+		while (st < len) {
+			char c = val.charAt(len - 1);
+			if (c > ' ' && !ArrayUtil.in(list, c))
+				break;
+			len--;
+		}
+		return ((st > 0) || (len < val.length())) ? val.substring(st, len) : val;
+	}
+}
