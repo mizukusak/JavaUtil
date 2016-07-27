f44efac1d9baba57f4cc1646f776deaffff074be
fumiaki shimazu <blood.opposer@gmail.com>
2016-07-27 12:21:19 +0900

    Create ArrayUtil.java

---
+++ ArrayUtil.java
@@ -0,0 +1,31 @@
+class ArrayUtil {
+  /**
+	 * array に 値が含まれるか検査
+	 * @param list
+	 * @param val
+	 * @return
+	 */
+	public static <T> boolean in(T[] list, T val) {
+		for (T v : list) {
+			if (v.equals(val)) {
+				return true;
+			}
+		}
+		return false;
+	}	public static boolean in(char[] list, char val) {
+		for (char v : list) {
+			if (v == val) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean in(int[] list, int val) {
+		for (int v : list) {
+			if (v == val) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
