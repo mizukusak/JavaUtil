65e3e321a70373ac61513d9d6806b2da979977c2
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-10 20:02:14 +0900

    Update ConsumerFunction.java

--- ConsumerFunction.java
+++ ConsumerFunction.java
@@ -3,7 +3,8 @@ import java.util.function.Function;
 /**
  * Stream 内での例外をラッピング(必ず Stream の外側で catch すること)
  */
-interface ConsumerFunction<T, R> extends Function<T, R> {
+@FunctionalInterface
+interface FunctionWrapper<T, R> extends Function<T, R> {
 
 	R applyOrigin(T t) throws Exception;
 
@@ -16,14 +17,15 @@ interface ConsumerFunction<T, R> extends Function<T, R> {
 		}
 	}
 
-	public static <T, R> ConsumerFunction<T, R> wrapFunction(ConsumerFunction<T, R> f) {
+	public static <T, R> FunctionWrapper<T, R> wrapFunction(FunctionWrapper<T, R> f) {
 		return f;
 	}
 }
+
 /*
 How to use:
 Stream.concat(Stream.of(folder), Stream.of(subFolders))
-	.flatMap(ConsumerFunction.wrapFunction(deleteFolder -> {
+	.flatMap(FunctionWrapper.wrapFunction(deleteFolder -> {
 		return Stream.of(deleteFolder.getMessages());
 	}));
 
