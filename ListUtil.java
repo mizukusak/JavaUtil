c2d28ebdb037a94029c44b27906425c13d05befc
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-19 16:26:11 +0900

    Update ListUtil.java

--- ListUtil.java
+++ ListUtil.java
@@ -1,14 +1,5 @@
 public class ListUtil {
 	/**
-	 * array を List に convert して返す
-	 * @param array
-	 * @return
-	 */
-	public static <T> List<T> toList(T[] array) {
-		return Stream.of(array).collect(Collectors.toList());
-	}
-
-	/**
 	 * 最後の要素を削除して元の List を返す
 	 * @param list
 	 * @return
