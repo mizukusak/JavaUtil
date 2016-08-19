87870a58175a926d9f9a13c7aedb6670077df42c
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-19 16:23:49 +0900

    Update ArrayUtil.java

--- ArrayUtil.java
+++ ArrayUtil.java
@@ -1,31 +1,38 @@
 public class ArrayUtil {
-  /**
+	/**
+	 * array を List にして返す
+	 * @param array
+	 * @return
+	 */
+	public static <T> List<T> toList(T[] array) {
+		return Stream.of(array).collect(Collectors.toList());
+	}
+
+	/**
+	 * array を List にして返す
+	 * @param array
+	 * @return
+	 */
+	public static <T> List<String> toStrList(T[] array) {
+		return Stream.of(array).map(v -> v.toString()).collect(Collectors.toList());
+	}
+
+	/**
 	 * array に 値が含まれるか検査
-	 * @param list
+	 * @param array
 	 * @param val
 	 * @return
 	 */
-	public static <T> boolean in(T[] list, T val) {
-		for (T v : list) {
-			if (v.equals(val)) {
-				return true;
-			}
-		}
-		return false;
-	}	public static boolean in(char[] list, char val) {
-		for (char v : list) {
-			if (v == val) {
-				return true;
-			}
-		}
-		return false;
+	public static <T> boolean in(T[] array, T val) {
+		return Stream.of(array).anyMatch(v -> v.equals(val));
 	}
-	public static boolean in(int[] list, int val) {
-		for (int v : list) {
-			if (v == val) {
-				return true;
-			}
-		}
-		return false;
+	public static boolean in(char[] array, char val) {
+		return IntStream.range(0, array.length).anyMatch(i -> array[i] == val);
+	}
+	public static boolean in(int[] array, int val) {
+		return IntStream.range(0, array.length).anyMatch(i -> array[i] == val);
+	}
+	public static boolean in(long[] array, long val) {
+		return IntStream.range(0, array.length).anyMatch(i -> array[i] == val);
 	}
 }
