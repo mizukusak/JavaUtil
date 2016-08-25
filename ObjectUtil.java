6768367e7c1529af02aa7836c0a0401dc1ecdb39
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-25 18:04:03 +0900

    Create ObjectUtil.java

---
+++ ObjectUtil.java
@@ -0,0 +1,49 @@
+public class ObjectUtil {
+	/**
+	 * Object の property に再帰的に処理を施す
+	 * @param obj
+	 * @param func
+	 * @return
+	 * @throws IllegalAccessException 
+	 * @throws IllegalArgumentException 
+	 */
+    public static <T extends Object, U> T deepMap(T obj, Function<Object, Object> func) throws IllegalArgumentException, IllegalAccessException {
+    	return _deepMap(obj, func, true);
+    }
+
+	/** {@link ObjectUtil#deepMap(T, Function)} */
+    public static <T extends Object, U> T _deepMap(T obj, Function<Object, Object> func, boolean isValid) 
+    		throws IllegalArgumentException, IllegalAccessException
+    {
+    	if (obj instanceof List<?>){
+    		List<U> list = (List<U>) obj;
+			int len = list.size();
+    		for (int i = 0; i < len; i++) {
+    			list.set(i, _deepMap(list.get(i), func, isValid));
+    		}
+    	}
+    	else if (obj instanceof Map<?, ?>){
+    		for (Entry<?, U> value : ((Map<?, U>) obj).entrySet()) {
+    			value.setValue(_deepMap(value.getValue(), func, isValid));
+    		}
+    	}
+    	else if(obj instanceof Object && isValid) {
+    		Field[] fileds = obj.getClass().getDeclaredFields();
+    		for (Field field : fileds) {
+    			field.setAccessible(true);
+    			if (Modifier.isFinal(field.getModifiers())) {
+    				continue;
+    			}
+    			
+				Object _deepMap = _deepMap(field.get(obj), func, field.getAnnotation(Valid.class) != null);
+				field.set(obj, _deepMap);	
+    		}
+    	}
+    	else {
+    		return (T) func.apply(obj);
+    	}
+    	return obj;
+    }
+
+}
+
