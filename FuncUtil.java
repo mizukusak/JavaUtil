b640f949cd3b6f7930202fa78e23dce0e6a65668
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-25 17:37:24 +0900

    Update and rename FunctionWrapper.java to FuncUtil.java

---
+++ FuncUtil.java
@@ -0,0 +1,19 @@
+
+public class FuncUtil {
+	/**
+	 * Stream 内での例外をラッピング(必ず Stream の外側で catch すること)
+	 */
+	public static <T, R> FunctionWrapper<T, R> wrapFunction(FunctionWrapper<T, R> f) {
+		return f;
+	}	
+}
+
+/*
+How to use:
+Stream.concat(Stream.of(folder), Stream.of(subFolders))
+	.flatMap(FunctionWrapper.wrapFunction(deleteFolder -> {
+		return Stream.of(deleteFolder.getMessages());
+	}));
+
+*/
+
