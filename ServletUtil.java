929e14986681744983e80455cebaf78f0841b975
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-26 09:16:49 +0900

    Create ServletUtil.java

---
+++ ServletUtil.java
@@ -0,0 +1,14 @@
+public class ServletUtil {
+	/**
+	 * convert MultipartFile to File 
+	 * @param multipartFile
+	 * @return
+	 */
+	private File getTempFile(MultipartFile multipartFile) {
+	    CommonsMultipartFile commonsMultipartFile = (CommonsMultipartFile) multipartFile;
+	    FileItem fileItem = commonsMultipartFile.getFileItem();
+	    DiskFileItem diskFileItem = (DiskFileItem) fileItem;
+	    String absPath = diskFileItem.getStoreLocation().getAbsolutePath();
+	    return new File(absPath);
+	}
+}
