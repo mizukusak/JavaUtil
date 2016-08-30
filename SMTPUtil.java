5f1d0ff1111fd9e70ea8aa3edb1e32ef09862430
fumiaki shimazu <blood.opposer@gmail.com>
2016-08-30 18:34:23 +0900

    Update SMTPUtil.java

--- SMTPUtil.java
+++ SMTPUtil.java
@@ -1,6 +1,6 @@
 public class SMTPUtil {
 
-	private static void addAttachment(Multipart multipart, File file, String name) throws MessagingException {
+	public static void addAttachment(Multipart multipart, File file, String name) throws MessagingException {
 	    DataSource source = new FileDataSource(file);
 	    BodyPart messageBodyPart = new MimeBodyPart();        
 	    messageBodyPart.setDataHandler(new DataHandler(source));
